cadfc389694dbe50d6ff1b69dfccdcec8594ca7f
ChanManChan <ngopal253@gmail.com>
2020-08-14 17:02:05 +0530

    changes-1

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
+// const core = require("@actions/core");
+// const github = require("@actions/github");
+// const { Octokit } = require("@octokit/rest");
+
+
+// async function run() {
+//   try {
+//     // we called it "token" in action.yml
+//     const token = core.getInput("token");
+//     const title = core.getInput("title");
+//     const body = core.getInput("body");
+//     const assignees = core.getInput("assignees");
+
+//     // const octokit = new github.GitHub(token);
+
+//     const octokit = new Octokit({
+//       auth: token
+//     })
+
+//     const response = await octokit.issues.create({
+//       // owner: github.context.repo.owner,
+//       // repo: github.context.repo.repo,
+//       ...github.context.repo,
+//       title,
+//       body,
+//       assignees: assignees ? assignees.split("\n") : undefined
+//     });
+
+//     core.setOutput("issue", JSON.stringify(response.data));
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
+
+// run();
+
+
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { Octokit } = require("@octokit/rest");
-
 
 async function run() {
   try {
-    // we called it "token" in action.yml
     const token = core.getInput("token");
     const title = core.getInput("title");
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    // const octokit = new github.GitHub(token);
-
-    const octokit = new Octokit({
-      auth: token
-    })
+    const octokit = new github.GitHub(token);
 
     const response = await octokit.issues.create({
       // owner: github.context.repo.owner,
@@ -33,3 +63,4 @@ async function run() {
 }
 
 run();
+
